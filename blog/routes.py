4863c04857139d07fe11304fcf828d2430b85286
Marlena Gorka <marlenagorka.mg@gmail.com>
2023-04-01 21:56:27 +0200

    add login and logout route

--- blog/routes.py
+++ blog/routes.py
@@ -1,7 +1,16 @@
 from blog import app
-from flask import render_template, request, flash
+from flask import render_template, request, session, flash, redirect, url_for
 from blog.models import Entry, db
-from blog.forms import EntryForm
+from blog.forms import EntryForm, LoginForm
+import functools
+
+def login_required(view_func):
+   @functools.wraps(view_func)
+   def check_permissions(*args, **kwargs):
+       if session.get('logged_in'):
+           return view_func(*args, **kwargs)
+       return redirect(url_for('login', next=request.path))
+   return check_permissions
 
 @app.route("/")
 def index():
@@ -10,17 +19,37 @@ def index():
 
 
 @app.route('/new-post/', methods=['GET', 'POST'])
+@login_required
 def create_entry():
     return create_or_edit_entry()
 
 
 @app.route('/edit-post/<int:entry_id>', methods=['GET', 'POST'])
+@login_required
 def edit_entry(entry_id: int):
     return create_or_edit_entry(entry_id=entry_id)
 
+@app.route('/delete/<int:entry_id>', methods=['POST'])
+@login_required
+def delete_entry(entry_id: int):
+    if request.method == 'POST':
+        entry = Entry.query.get(entry_id)
+        if entry:
+            db.session.delete(entry)
+            db.session.commit()
+            flash('Post deleted', 'success')
+            return redirect(url_for('index'))
+        else:
+            flash(f'No Post ID: {entry_id}')
+            return redirect(url_for('index'))
+
+@app.route("/drafts/", methods=['GET'])
+@login_required
+def list_drafts():
+   drafts = Entry.query.filter_by(is_published=False).order_by(Entry.pub_date.desc())
+   return render_template("drafts.html", drafts=drafts)
 
 def create_or_edit_entry(**kwargs):
-    # GET
     entry_id = kwargs.pop('entry_id', None)
     form = EntryForm()
     entry = None
@@ -29,14 +58,13 @@ def create_or_edit_entry(**kwargs):
         entry = Entry.query.filter_by(id=entry_id).first_or_404()
         form = EntryForm(obj=entry)
 
-    # POST
     if request.method == 'POST':
         if form.validate_on_submit():
             if entry_id:
                 form.populate_obj(entry)
                 db.session.commit()
-                flash('Zmiany w poscie zostały zapisane. '
-                      'Aby wyświetlić go na stronie głównej pamiętaj aby zaznaczyć "Wpis opublikowany"')
+                flash('Post edited.'
+                    )
             else:
                 entry = Entry(
                     title=form.title.data,
@@ -45,8 +73,34 @@ def create_or_edit_entry(**kwargs):
                 )
                 db.session.add(entry)
                 db.session.commit()
-                flash('Nowy post został dodany. '
-                      'Aby wyświetlić go na stronie głównej pamiętaj aby zaznaczyć "Wpis opublikowany"')
+                flash('Post added. '
+                      )
         else:
             errors = form.errors
     return render_template('entry_form.html', form=form, errors=errors)
+
+@app.route("/login/", methods=['GET', 'POST'])
+def login():
+   form = LoginForm()
+   errors = None
+   next_url = request.args.get('next')
+   if request.method == 'POST':
+       if form.validate_on_submit():
+           session['logged_in'] = True
+           session.permanent = True  # Use cookie to store session.
+           flash('You are now logged in.', 'success')
+           return redirect(next_url or url_for('index'))
+       else:
+           errors = form.errors
+   return render_template("login_form.html", form=form, errors=errors)
+
+
+@app.route('/logout/', methods=['GET', 'POST'])
+def logout():
+   if request.method == 'POST':
+       session.clear()
+       flash('You are now logged out.', 'success')
+   return redirect(url_for('index'))
+
+
+
